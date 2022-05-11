af42a4b330ec593d3c1586ba09aae5314b9e7b64
Paulo Dutra <paulodutra75@gmail.com>
2022-05-11 11:31:00 -0300

    Estrutura de pastas e módulos foi reorganizada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'movies', loadChildren: () => import('./views/movies/movies.module').then(m => m.MoviesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
