9797ec4984657db533119e90631a41da5c09ac42
Paulo Dutra <paulodutra75@gmail.com>
2023-10-12 22:56:24 -0300

    ajustes na estilização e usabilidade

--- src/app/views/movies/movie-search-input/movie-search-input.component.ts
+++ src/app/views/movies/movie-search-input/movie-search-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { take } from 'rxjs';
 import { Result } from 'src/app/models/Movies';
 import { MoviesService } from 'src/app/services/movies.service';
@@ -8,20 +9,24 @@ import { MoviesService } from 'src/app/services/movies.service';
   templateUrl: './movie-search-input.component.html',
   styleUrls: ['./movie-search-input.component.css']
 })
-export class MovieSearchInputComponent implements OnInit {
-  inputText = '';
+export class MovieSearchInputComponent {
   movies?: Result[];
 
-  constructor(private moviesService: MoviesService) { }
-
-  ngOnInit(): void {
+  constructor(private moviesService: MoviesService, private router: Router) { }
+  
+  getOptionText(option: Result) {
+    return option?.title;
   }
 
-  handleInput() {
-    if(this.inputText != '') {
-      this.moviesService.getMoviesByTitle(this.inputText)
+  handleInput(inputText: any) {
+    if(inputText.value != '') {
+      this.moviesService.getMoviesByTitle(inputText.value)
         .pipe(take(1))
         .subscribe((data) => this.movies = data.results);
     }
+  }
+
+  handleOption(movie: Result) {
+    this.router.navigate(['/movie-details', movie.id]);
   }
 }
